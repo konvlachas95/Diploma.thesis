1a5345837d329acc9d1116e12de85650db2195c6
Kos <kostas@hotmail.gr>
2023-07-05 18:43:20 +0300

    Update 0_run_Figures1-2.py
    g = 0.1 fixed . for b = 0.3 / 1.0 / 2.0
    we get fig 1/2/3 respectively
    for each fig we run the script with diff
    J[J1,J2,J3]

--- 0_run_Figures1-2.py
+++ 0_run_Figures1-2.py
@@ -12,7 +12,7 @@ import networkx as nx
 ########################
 
 
-# Figure 1a (upper left)
+# Figure 1
 #n = 11
 #beta = 0.3
 #gamma = 0.1
@@ -23,7 +23,7 @@ import networkx as nx
 #lattice = "lattice_von_neumann"
 #node_size = 100
 
-# Figure 1b (upper right)
+# Figure 2
 #n = 11
 #beta = 1.0
 #gamma = 0.1
@@ -34,38 +34,16 @@ import networkx as nx
 #lattice = "lattice_von_neumann"
 #node_size = 100
 
-# Figure 1c (lower left)
+# Figure 3
 #n = 11
 #beta = 2.0
 #gamma = 0.1
 #N_tags = 1
-#J0 = [ 4, 1, 4 ]
+#J0 = [ 1, 4, 1 ]
 #N_epochs = 2000
 #num_runs = 1
 #lattice = "lattice_von_neumann"
 #node_size = 100
-
-# Figure 1d (lower right)
-#n = 11
-#beta = 2.0
-#gamma = 0.1
-#N_tags = 1
-#J0 = [ 4, 4, 4 ]
-#N_epochs = 2000
-#num_runs = 1
-#lattice = "lattice_von_neumann"
-#node_size = 100
-
-# Figure 2a (upper plot)
-n = 11
-beta = 2.0
-gamma = 0.1
-N_tags = 1
-J0 = [ 1, 4, 1 ]
-N_epochs = 2000
-num_runs = 1
-lattice = "lattice_von_neumann"
-node_size = 100
 
 G = gr.lattice_von_neumann(n)
 
